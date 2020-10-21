ac98cc07f05255f6868f187c9fd13d4d2c47ad85
wildwing <ggipk1010@gmail.com>
2020-10-21 17:24:40 +0900

    깃 만들기

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  const name= "react";
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, // 기본 단위 px
+    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+  }
+  return (
+    <>
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
+  );
+}
+
+export default App;
